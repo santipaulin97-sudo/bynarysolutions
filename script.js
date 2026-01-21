40152ba0f6d04404e743ec1b8e408ca2747876a5
Santiago Paulin <santipaulin97@gmail.com>
2026-01-21 15:55:49 -0300

    Implement smooth scroll for internal anchor links
    Add smooth scroll functionality for internal links.

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ langBtn.addEventListener('click', () => {
         el.innerText = el.getAttribute(`data-${currentLang}`);
     });
 });
+
+// Smooth scroll para los links internos
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
